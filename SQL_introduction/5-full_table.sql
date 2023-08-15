8d386fc2d8a92c71b81df4c09111df0815ba4f3f
Maester-jeff <jeffndungu45@gmail.com>
2023-08-15 16:06:51 +0300

    5-full_tables.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,7 @@
 -- Script that prints the full description of the table first_table from the database hbtn_0c_0
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 -- Creating the database to be used as current.
-USE hbtn_0c_0
+USE hbtn_0c_0;
 -- Creating a table in the database.
 CREATE TABLE iF NOT EXISTS first_table (id iNT, name VARCHAR(256));
 -- Describing features of first_table.
