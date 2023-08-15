d0f00da7a21aa3940bee48afd2903d623fb28519
Maester-jeff <jeffndungu45@gmail.com>
2023-08-15 22:55:34 +0300

    7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,10 +1,9 @@
 -- Script that inserts a new row in the table first_table (database hbtn_0c_0).
-
 -- Using the database as current.
 USE hbtn_0c_0;
 
 -- Inserting a new row.
 INSERT INTO first_table (id, name)
-VALUES (89, 'Holberton School');
+VALUES ('89', 'Holberton School');
 -- Display a new row
 SELECT * FROM first_table;
