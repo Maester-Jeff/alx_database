d446d84425bfaedc0b8e0d339d5d68fceaa99549
Maester-jeff <jeffndungu45@gmail.com>
2023-08-15 22:51:44 +0300

    7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,19 +1,8 @@
 -- Script that inserts a new row in the table first_table (database hbtn_0c_0).
--- Creating the database to be used as current.
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
 -- Using the database as current.
 USE hbtn_0c_0;
 
--- Creating a table in the database.
-CREATE TABLE IF NOT EXISTS first_table 
-(
-id iNT, 
-name VARCHAR(128)
-);
-
--- Including records (rows) into the two columns.
-INSERT INTO first_table
-VALUES('1', 'First name'), ('2', 'Last name'), ('3', 'name');
+-- Inserting a new row.
 INSERT INTO first_table (id, name)
-VALUES ('89', 'Holberton School');
+VALUES (89, 'Holberton School');
