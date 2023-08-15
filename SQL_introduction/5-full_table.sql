36243eeccce86ecaaa69edf60d9e91fa4adb90d6
Maester-jeff <jeffndungu45@gmail.com>
2023-08-15 16:49:02 +0300

    5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -4,6 +4,11 @@ CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 -- Using the database as current.
 USE hbtn_0c_0;
 -- Creating a table in the database.
-CREATE TABLE first_table (id iNT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table 
+(
+id iNT, 
+name VARCHAR(256)
+);
+USE hbtn_0c_0;
 -- Describing features of first_table.
-DESC first_table;
+SHOW COLUMNS FROM first_table;
