d82523f4fcd6a8a4a361b38472e7b62a564ba6c2
Maester-jeff <jeffndungu45@gmail.com>
2023-08-15 23:16:39 +0300

    8-count_89.sql

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -7,5 +7,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
 
 -- Displaying the number of records.
-SELECT COUNT (*) FROM first_table
-WHERE id = 89;
+SELECT COUNT (*) FROM first_table WHERE id = 89;
