5283c43678b19792b8a4be7a9e9299746f64bfb1
Maester-jeff <jeffndungu45@gmail.com>
2023-08-15 16:00:16 +0300

    5-full

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,6 @@
 -- Script that prints the full description of the table first_table from the database hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- Invoking the database to be used as current.
 USE hbtn_0c_0
+-- Creating a table in the database
 DESC first_table;
