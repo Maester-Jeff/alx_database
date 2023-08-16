0d3426aa50cb54c08b191cc258c06c3fc52d4ab8
Maester-jeff <jeffndungu45@gmail.com>
2023-08-16 15:02:01 +0300

    8

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -3,6 +3,9 @@ in the table first_table of the database hbtn_0c_0. */
 -- Creating the database to be used as current.
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
+-- Using the database as current.
+USE hbtn_0c_0;
+
 -- Creating a table in the database.
 CREATE TABLE IF NOT EXISTS first_table 
 (
@@ -10,9 +13,15 @@ id iNT,
 name VARCHAR(128)
 );
 
+-- Using the database as current.
+USE hbtn_0c_0;
+
 -- Inserting records into the table.
 INSERT INTO first_table (id, name)
 VALUES ('89', 'Holberton School'), ('89', 'Holberton School'), ('89', 'Holberton School');
+
+-- Using the database as current.
+USE hbtn_0c_0;
 
 -- Displaying the number of records.
 SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
