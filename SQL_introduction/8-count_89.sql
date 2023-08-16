1e8f7a2b34143ae823306eec43a2e2a24d8af6a0
Maester-jeff <jeffndungu45@gmail.com>
2023-08-16 14:31:35 +0300

    8-count_89.sql

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,5 +1,10 @@
 /* Script that displays the number of records with id = 89 
 in the table first_table of the database hbtn_0c_0. */
+-- Creating the database to be used as current.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Using the database as current.
+USE hbtn_0c_0;
 
 -- Displaying the number of records.
-SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
