091e5f8e989d1eff4db6bfaedd5ad17967347642
Maester-jeff <jeffndungu45@gmail.com>
2023-08-16 14:43:46 +0300

    8

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -9,10 +9,8 @@ USE hbtn_0c_0;
 -- Creating a table in the database.
 CREATE TABLE IF NOT EXISTS first_table 
 (
-id iNT AUTO_INCREMENT PRIMARY KEY, 
-name VARCHAR(128) DEFAULT NULL,
-c CHAR(1) DEFAULT NULL,
-created_at DATE DEFAULT NULL
+id iNT, 
+name VARCHAR(128) NULL
 );
 
 INSERT INTO first_table (id, name)
