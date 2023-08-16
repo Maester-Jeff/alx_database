d7cae6ec452c6ad4662e74d3c12b640da3563990
Maester-jeff <jeffndungu45@gmail.com>
2023-08-16 14:30:11 +0300

    8-count_89.sql

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -2,4 +2,4 @@
 in the table first_table of the database hbtn_0c_0. */
 
 -- Displaying the number of records.
-SELECT COUNT (*) FROM hbtn_0c_0.first_table WHERE id = 89;
+SELECT COUNT(*) FROM hbtn_0c_0.first_table WHERE id = 89;
