bdf33e116752cc181892c2c220983c24b1e996b8
Maester-jeff <jeffndungu45@gmail.com>
2023-08-16 14:13:57 +0300

    6-list_values.sql

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,8 +1,4 @@
 -- Script that lists all rows of the table first_table from the database hbtn_0c_0.
 
--- Including records (rows) into the two columns.
-INSERT INTO first_table
-VALUES('1', 'First name'), ('2', 'Last name'), ('3', 'name');
-
 -- Displaying the rows.
 SELECT *FROM first_table;
