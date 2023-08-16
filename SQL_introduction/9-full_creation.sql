821ecc5992dde77bd56e03338a13f4d3255ccc72
Maester-jeff <jeffndungu45@gmail.com>
2023-08-16 15:26:55 +0300

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,14 +1,8 @@
 /* Script that creates a table second_table in the database
 hbtn_0c_0 in your MySQL server and add multiples rows. */
 
--- Creating the database to be used as current.
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Using the database as current.
-USE hbtn_0c_0;
-
 -- Creating a table in the database.
-CREATE TABLE IF NOT EXISTS second_table 
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table 
 (
 id iNT, 
 name VARCHAR(256),
