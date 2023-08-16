6af62c62236a3580df856b0c6de7bbc41244a0c6
Maester-jeff <jeffndungu45@gmail.com>
2023-08-16 14:12:02 +0300

    6-list_values.sql

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,16 +1,4 @@
 -- Script that lists all rows of the table first_table from the database hbtn_0c_0.
--- Creating the database to be used as current.
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Using the database as current.
-USE hbtn_0c_0;
-
--- Creating a table in the database.
-CREATE TABLE IF NOT EXISTS first_table 
-(
-id iNT, 
-name VARCHAR(128)
-);
 
 -- Including records (rows) into the two columns.
 INSERT INTO first_table
