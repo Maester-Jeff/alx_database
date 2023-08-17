6981056f8441fd3a19d45eedf0ce6f64d9dfd2ad
Maester-jeff <jeffndungu45@gmail.com>
2023-08-17 23:05:19 +0300

    6-states.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,14 @@
+/* script that creates the database hbtn_0d_usa and
+the table states (in the database hbtn_0d_usa). */
+-- Creating the database.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Using the database as current.
+USE hbtn_0d_usa;
+
+-- Creating the table.
+CREATE TABLE IF NOT EXISTS states
+(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
