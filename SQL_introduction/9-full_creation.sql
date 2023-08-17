6abc8414f0d8b0327e1afed7e946bc0fd49c6cff
Maester-jeff <jeffndungu45@gmail.com>
2023-08-17 16:30:37 +0300

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -9,11 +9,14 @@ USE hbtn_0c_0;
 -- Creating a table in the database.
 CREATE TABLE IF NOT EXISTS second_table
 (
-id iNT, 
-name VARCHAR(256),
-score iNT
+    id iNT, 
+    name VARCHAR(256),
+    score iNT
 );
 
 -- Inserting records.
-INSERT INTO second_table (id, name, score)
-VALUES (1, 'John', 10), ('2', 'Alex', '3'), ('3', 'Bob', '14'), ('4', 'George', '8');
+INSERT INTO second_table (id, name, score) VALUES 
+    (1, 'John', 10), 
+    (2, 'Alex', 3), 
+    (3, 'Bob', 14), 
+    (4, 'George', 8);
