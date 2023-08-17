aeb6b848ed74523bc510f1f12beeda1a54aced3c
Maester-jeff <jeffndungu45@gmail.com>
2023-08-17 22:20:20 +0300

    1-create_users.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,9 @@
 -- Script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
 -- Setting the password.
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+SET PASSWORD FOR 'user_0d_1'@'localhost'="user_0d_1_pwd";
+
+-- Granting all the privileges.
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
