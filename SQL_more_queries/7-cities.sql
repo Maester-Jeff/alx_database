43a1a681b30a55d01c94b7bd3fb56fe84e183f82
Maester-jeff <jeffndungu45@gmail.com>
2023-08-17 23:16:49 +0300

    7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -10,5 +10,5 @@ CREATE TABLE IF NOT EXISTS cities
     id AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(256),
     state_id INT NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
