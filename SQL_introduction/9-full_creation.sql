336a0e6361ccc2e4127de175adacc2d57cf323e6
Maester-jeff <jeffndungu45@gmail.com>
2023-08-17 20:09:40 +0300

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS second_table
 );
 
 -- Inserting records.
-INSERT INTO second_table (id, name, score) VALUES 
+INSERT INTO second_table (id, name, score) 
+VALUES 
     (1, 'John', 10), 
     (2, 'Alex', 3), 
     (3, 'Bob', 14), 
