f69fcb008fe9103e720dc4699e205504936d8c7d
Maester-jeff <jeffndungu45@gmail.com>
2023-08-17 22:54:32 +0300

    5-unique_id.sql

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,7 @@
+-- Script that creates the table unique_id.
+-- Creating the table.
+CREATE TABLE IF NOT EXISTS unique_id
+(
+    id INT AUTO_INCREMENT PRIMARY KEY DEFAULT 1,
+    name VARCHAR(256)
+);
